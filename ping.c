07c4e20b71e95c10f2a35f21ebe8d13c99f688a6
aBlackPigeon <ablackpigeon7@gmail.com>
2026-03-24 12:07:37 +0000

    -Raw Socket & Destination Arguments

--- ping.c
+++ ping.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/ip_icmp.h>
 
-int main(){
-    printf("Ping Started ... \n");
+
+int main(int argc , char *argv[]){
+
+    if(argc != 2){
+        printf("Usage : %s <IP_ADDRESS> ", argv[0]);
+        return 1;
+    }
+
+    char *target_ip = argv[1];
+
+    int sockfd = socket(AF_INET,SOCK_RAW,IPPROTO_ICMP);
+    if(sockfd < 0){
+        perror("Socket Creation failed");
+        return 1;
+    }
+
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = 0;
+
+    if(inet_pton(AF_INET,target_ip,&dest_addr.sin_addr) <= 0){
+        printf("Invalid Address");
+        return 1;
+    }
+
+    printf("Raw Socket Created Successfully\n");
+    printf("Target IP : %s\n", target_ip);
+
     return 0;
 }
 
